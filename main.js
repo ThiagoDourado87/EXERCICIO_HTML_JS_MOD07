9b4a2f3031441f3cb5fd535ccccb2e2f5f2557a1
Thiago Dourado <thiagodourado@gmail.com>
2023-02-07 20:16:11 -0300

    Correção do java script

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
-const form =document.getElementById('valida-numero')
+const form = document.getElementById('valida-numero')
 
 
 
 
-form.addEventListener('submit', function(e){
+form.addEventListener('submit', function (e) {
     e.preventDefault();
-    const numeroA=(document.getElementById('numA'));
-    const numeroB=(document.getElementById('numB'));
-    const mensagemSuccess =`O número B <b>( ${numeroB.value})</b> é maior que o número A <b>( ${numeroA.value})</b>)`;
-    const mensagemErro = `O número B <b>( ${numeroB.value}) NÃO É </b> maior que o número A <b>( ${numeroA.value})</b>)`;
+    const numeroA = parseFloat((document.getElementById('numA').value));
+    const numeroB = parseFloat((document.getElementById('numB').value));
+    const mensagemSuccess = `O número B <b>( ${numeroB})</b> é maior que o número A <b>( ${numeroA})</b>)`;
+    const mensagemErro = `O número B <b>( ${numeroB}) NÃO É </b> maior que o número A <b>( ${numeroA})</b>)`;
     const containerMensagemSucesso = document.querySelector('.success-message')
     const containerMensagemErro = document.querySelector('.error-message')
-    
-    if (numeroB.value>numeroA.value){
-    
-    containerMensagemSucesso.innerHTML=mensagemSuccess
-    containerMensagemSucesso.style.display='block'
-    containerMensagemErro.style.display='none'
-    }else{
-        
-        containerMensagemErro.innerHTML=mensagemErro
-        containerMensagemErro.style.display='block'
-        containerMensagemSucesso.style.display='none'
-    
-}})
+
+    if (numeroB > numeroA) {
+
+        containerMensagemSucesso.innerHTML = mensagemSuccess
+        containerMensagemSucesso.style.display = 'block'
+        containerMensagemErro.style.display = 'none'
+    } else {
+
+        containerMensagemErro.innerHTML = mensagemErro
+        containerMensagemErro.style.display = 'block'
+        containerMensagemSucesso.style.display = 'none'
+
+    }
+})
