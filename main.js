7ac823a5c6efcfe1f8375b0490db93ff3fbedd96
Thiago Dourado <thiagodourado@gmail.com>
2023-02-06 21:51:35 -0300

    ajust main.js

--- main.js
+++ main.js
@@ -1,24 +1,14 @@
-const form = document.getElementById('valida-numero');
+const form =document.getElementById('valida-numero')
 
-function maior(numeroA,numeroB){
-        var numeroA = parseFloat(document.getElementById("numA").value);
-        var numeroB = parseFloat(document.getElementById("numB").value);
-        const mensagemSucesso= `O número B '<b>(${numeroB})</b> 'é maior que o Número A: <b>(${numeroA}</b>.`;
-        const mensagemErro=`O Número B <b>(${numeroB})</b> é maior que o Número A: <b>(${numeroA}</b>.)`;
-        
-        if (numeroB > numeroA)
-            /*alert("O número B ("+numeroB+") é maior que o número A (" + numeroA+").");*/
-            document.querySelector('.sucess-message').innerHTML=mensagemSucesso;
-        else
-            /*alert("O número B ("+numeroB+ ") não é maior que o número A ("+ numeroA+").");*/
-            document.querySelector('.error-message').innerHTML=mensagemErro;
+function validaMaior() {
+    const numeroA=parseFloat(document.getElementById('numA'));
+    const numeroB=parseFloat(document.getElementById('numB'));
+    const mensagemSucess =`O número B <b>( ${numeroB})</b> é maior que o número A <b>( ${numeroA})</b>)`;
+    const mensagemErro = `O número B <b>( ${numeroB}) NÃO É </b> maior que o número A <b>( ${numeroA})</b>)`;
+    numeroB>numeroA ? document.querySelector('.success-message').innerHTML = mensagemSucess :document.querySelector('.error-message').innerHTML = mensagemErro;
 }
 
 form.addEventListener('submit', function(e){
-
-    e.preventDefault();
-    maior();
-}
-)
-
-console.log (form);
+    e.preventDefault
+    validaMaior()
+})
