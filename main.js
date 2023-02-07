0b336fffd8f4ef742e33e864cb87746ad8d01c6d
Thiago Dourado <thiagodourado@gmail.com>
2023-02-07 20:18:26 -0300

    ajuste letras js

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ form.addEventListener('submit', function (e) {
     e.preventDefault();
     const numeroA = parseFloat((document.getElementById('numA').value));
     const numeroB = parseFloat((document.getElementById('numB').value));
-    const mensagemSuccess = `O número B <b>( ${numeroB})</b> é maior que o número A <b>( ${numeroA})</b>)`;
-    const mensagemErro = `O número B <b>( ${numeroB}) NÃO É </b> maior que o número A <b>( ${numeroA})</b>)`;
+    const mensagemSuccess = `O número B <b>(${numeroB})</b> é maior que o número A <b>(${numeroA})</b>).`;
+    const mensagemErro = `O número B <b>(${numeroB}) NÃO É </b> maior que o número A <b>(${numeroA})</b>.`;
     const containerMensagemSucesso = document.querySelector('.success-message')
     const containerMensagemErro = document.querySelector('.error-message')
 
